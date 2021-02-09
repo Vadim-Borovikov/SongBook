347bedcb7395ec33475453f23cd2d69b64661354
Vadim Borovikov <vadim.borovikov@gmail.com>
2021-02-09 11:40:26 +0300

    .NET Core MVC project added

---
+++ SongBook.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SongBook.Web.Models;
+
+namespace SongBook.Web.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index() => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SongBook.Web/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SongBook.Web
+{
+    public sealed class Startup
+    {
+        public Startup(IConfiguration config) => _config = config;
+
+        [SuppressMessage("ReSharper", "UnusedMember.Global")]
+        public void ConfigureServices(IServiceCollection services) => services.AddMvc();
+
+        [SuppressMessage("ReSharper", "UnusedMember.Global")]
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes => routes.MapRoute("update", $"{_config["Token"]}/{{controller=Update}}/{{action=post}}"));
+        }
+
+        private readonly IConfiguration _config;
+    }
+}
